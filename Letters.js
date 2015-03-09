aa1821ad05184c5b9680541eef7c516ea08658f9
holme22e <holme22e@mtholyoke.edu>
2015-03-09 19:41:49 -0400

    edited

--- Letters.js
+++ Letters.js
@@ -1,15 +1,12 @@
 
 function letterE(){
 	document.getElementById("spell").textContent += "		E";
-	index++;
 }
 
 function letterL(){
 	document.getElementById("spell").textContent += "		L";
-	index++;
 }
 
 function letterA(){
 	document.getElementById("spell").textContent += "		A";
-	index++;
 }
