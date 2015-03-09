22c7cc4d4967d786e8558c7d0a03b1f871bae167
holme22e <holme22e@mtholyoke.edu>
2015-03-09 17:07:32 -0400

    js file

---
+++ Letters.js
@@ -0,0 +1,15 @@
+
+function letterE(){
+	document.getElementById("spell").textContent += "		E";
+	index++;
+}
+
+function letterL(){
+	document.getElementById("spell").textContent += "		L";
+	index++;
+}
+
+function letterA(){
+	document.getElementById("spell").textContent += "		A";
+	index++;
+}
